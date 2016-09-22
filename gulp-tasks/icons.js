a541339238e9788f57681743fa2de138f9b6ee9a
Ben McClure <ben.mcclure@gmail.com>
2016-09-21 22:58:23 -0500

    Major rewrite of icons system, introduced assets via eyeglass module, bump to 0.4.0

--- gulp-tasks/icons.js
+++ gulp-tasks/icons.js
@@ -2,47 +2,61 @@
  * Created by BMcClure on 9/17/2016.
  */
 var svgSprite = require('gulp-svg-sprite');
+var svg2png = require('gulp-svg2png');
 var replace = require('gulp-replace');
 var del = require('del');
 var notify = require('gulp-notify');
+var size = require('gulp-size');
+var gutil = require('gulp-util');
 
 module.exports = function (gulp, config) {
     gulp.task('icons', ['icons:asset-urls']);
 
-    gulp.task('icons:sprites', function () {
+    gulp.task('icons:sprite', function () {
         if (!config.icons.enabled) {
             return;
         }
 
         var iconFiles = config.sources.icons || ['./src/icons/*.svg'];
         var iconDestination = config.icons.destination || "./";
+        var pngPath = config.paths.iconsPng || "images/icons.png";
+        var svgPath = config.paths.icons || "images/icons.svg";
 
         var spriteConfig = {
-            "mode": {},
-            "shape": {
-                "dimension": {
+            shape: {
+                dimension: {
                     maxWidth: 32,
                     maxHeight: 32
-                }
-            }
-        };
-
-        spriteConfig.mode[config.icons.mode || "view"] = {
-            "dest": iconDestination,
-            "sprite": config.paths.icons || "images/icons.svg",
-            "prefix": config.icons.prefix || ".icon--%s",
-            "mixin": config.icons.mixin || "icon-base",
-            "inline": false,
-            "bust": false,
-            "common": config.icons.commonClass || "icon",
-            "dimensions": true,
-            "render": {
-                "scss": {
-                    "dest": config.icons.cssFile || "./src/scss/generated/_icons.scss"
+                },
+                spacing: {
+                    padding: 5
                 }
             },
-            "example": {
-                "dest": config.icons.htmlPath || "docs/icons.html"
+            mode: {
+                css: {
+                    dest: iconDestination,
+                    layout: "diagonal",
+                    sprite: svgPath,
+                    //"prefix": config.icons.prefix || ".icon--%s",
+                    //"mixin": config.icons.mixin || "icon-base",
+                    bust: false,
+                    //"common": config.icons.commonClass || "icon",
+                    //"dimensions": true,
+                    render: {
+                        scss: {
+                            dest: config.icons.cssFile || "./src/scss/generated/_icons.scss",
+                            template: config.icons.scssTemplate || path.join(__dirname, 'templates/sprite-template.scss')
+                        }
+                    }
+                    //"example": {
+                    //    "dest": config.icons.htmlPath || "docs/icons.html"
+                    //}
+                }
+            },
+            variables: {
+                mapname: "icons",
+                pngPath: pngPath,
+                svgPath: svgPath
             }
         };
 
@@ -50,14 +64,26 @@ module.exports = function (gulp, config) {
             .pipe(svgSprite(spriteConfig))
             .pipe(gulp.dest(iconDestination))
             .pipe(notify({
-                title: "Icon Sprites Generated",
-                message: "All SVG icon sprites have been created.",
+                title: "Icon Sprite Generated",
+                message: "The SVG icon sprite has been generated.",
                 onLast: true
             }));
 
     });
 
-    gulp.task('icons:asset-urls', ['icons:sprites'], function () {
+    gulp.task('icons:png-sprite', ['icons:sprite'], function () {
+        var iconDestination = config.icons.destination || "./";
+        var icons = config.paths.icons || 'images/icons.svg';
+
+        return gulp.src(path.join(iconDestination, icons))
+            .pipe(svg2png())
+            .pipe(size({
+                showFiles: true
+            }))
+            .pipe(gulp.dest(config.paths.images || './images'))
+    });
+
+    gulp.task('icons:asset-urls', ['icons:png-sprite'], function () {
         var dir = "src/scss/generated";
         var cssFile = config.icons.cssFile || "src/scss/generated/_icons.scss";
 
