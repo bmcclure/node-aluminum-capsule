920f0a22fbe170eb10e36ab2924dea396f4c1cc9
Ben McClure <ben.mcclure@gmail.com>
2016-09-28 19:29:06 -0500

    First pass at rewriting the icon system to work with multiple icon sets, and various fixes for fonts task (may not be fully functional yet in either case)

--- gulp-tasks/fonts.js
+++ gulp-tasks/fonts.js
@@ -1,22 +1,51 @@
 /**
  * Created by BMcClure on 9/19/2016.
  */
-//var fontgen = require('gulp-fontgen');
 var path = require('path');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var notifier = require('node-notifier');
+var replace = require('gulp-replace');
 
 module.exports = function (gulp, config) {
-    gulp.task('fonts', function () {
+    gulp.task('fonts', ['fonts:sass'], function () {
         if (!config.fonts.enabled) {
             return;
         }
 
-        return;
+        notifier.notify({
+            title: "Web Fonts Generated",
+            message: "All web fonts have been regenerated."
+        });
+    });
 
-        /*return gulp.src(config.sources.fonts)
+    gulp.task('fonts:generate', function () {
+        if (!config.fonts.enabled) {
+            return;
+        }
+
+        // Load fontgen here since it has strict system requirements that shouldn't be needed if the task is disabled.
+        var fontgen = require('gulp-fontgen');
+
+        return gulp.src(config.sources.fonts)
             .pipe(fontgen({
                 dest: config.paths.fonts,
-                css: path.join(config.paths.generatedScss, config.fonts.cssFile),
                 css_fontpath: config.paths.fonts
-            }));*/
+            }));
+    });
+
+    gulp.task('fonts:sass', ['fonts:generate'], function () {
+        if (!config.fonts.enabled) {
+            return;
+        }
+
+        return gulp.src(path.join(config.paths.fonts, '*.css'))
+            .pipe(clean())
+            .pipe(replace(/(\s+)url\(([^)]+)\)/g, '$1asset-url($2)'))
+            .pipe(rename({
+                prefix: "_",
+                extname: ".scss"
+            }))
+            .pipe(gulp.dest(config.paths.fontsCss));
     });
 };
--- gulp-tasks/icons.js
+++ gulp-tasks/icons.js
@@ -8,71 +8,76 @@ var del = require('del');
 var notify = require('gulp-notify');
 var size = require('gulp-size');
 var path = require('path');
+var foreach = require('gulp-foreach');
 
 module.exports = function (gulp, config) {
-    gulp.task('icons', ['icons:png-sprite']);
+    gulp.task('icons', ['icons:png-sprites']);
 
-    gulp.task('icons:sprite', function () {
+    gulp.task('icons:sprites', function () {
         if (!config.icons.enabled) {
             return;
         }
 
         var defaultScssTemplate = path.join(path.dirname(__dirname), 'templates/sprite-template.scss.mustache');
 
-        var spriteConfig = {
-            shape: {
-                dimension: {
-                    maxWidth: config.icons.maxWidth,
-                    maxHeight: config.icons.maxHeight
-                },
-                spacing: {
-                    padding: config.icons.padding
-                }
-            },
-            mode: {
-                css: {
-                    dest: config.icons.destination,
-                    layout: config.icons.layout,
-                    sprite: config.paths.icons + '.svg',
-                    bust: config.icons.cacheBust,
-                    render: {
-                        scss: {
-                            dest: path.join(config.paths.generatedScss, config.icons.cssFile),
-                            template: config.icons.scssTemplate || defaultScssTemplate
+        return gulp.src(path.join(config.sources.icons, './*'), {base: config.sources.icons})
+            .pipe(foreach(function (stream, file) {
+                var spriteName = path.basename(file);
+                var spriteConfig = {
+                    shape: {
+                        dimension: {
+                            maxWidth: config.icons.maxWidth,
+                            maxHeight: config.icons.maxHeight
+                        },
+                        spacing: {
+                            padding: config.icons.padding
                         }
+                    },
+                    mode: {
+                        css: {
+                            dest: config.icons.destination,
+                            layout: config.icons.layout,
+                            sprite: path.normalize(path.join(config.paths.icons, './' + spriteName + '.svg')),
+                            bust: config.icons.cacheBust,
+                            render: {
+                                scss: {
+                                    dest: path.normalize(path.join(config.paths.iconsCss, './' + spriteName + config.icons.cssExt)),
+                                    template: config.icons.scssTemplate || defaultScssTemplate
+                                }
+                            }
+                            /* Removed until I get further along
+                             ,
+                             "common": config.icons.commonClass || "icon",
+                             "dimensions": true,
+                             "prefix": config.icons.prefix || ".icon--%s",
+                             "mixin": config.icons.mixin || "icon-base",
+                             "example": {
+                             "dest": config.icons.htmlPath || "docs/icons.html"
+                             }
+                             */
+                        }
+                    },
+                    variables: {
+                        mapname: "icons",
+                        pngPath: config.paths.icons + '.png',
+                        svgPath: config.paths.icons + '.svg'
                     }
-                    /* Removed until I get further along
-                    ,
-                     "common": config.icons.commonClass || "icon",
-                     "dimensions": true,
-                     "prefix": config.icons.prefix || ".icon--%s",
-                     "mixin": config.icons.mixin || "icon-base",
-                     "example": {
-                         "dest": config.icons.htmlPath || "docs/icons.html"
-                      }
-                    */
-                }
-            },
-            variables: {
-                mapname: "icons",
-                pngPath: config.paths.icons + '.png',
-                svgPath: config.paths.icons + '.svg'
-            }
-        };
+                };
 
-        return gulp.src(config.sources.icons)
-            .pipe(svgSprite(spriteConfig))
-            .pipe(gulp.dest(config.icons.destination))
+                return stream
+                    .pipe(svgSprite(spriteConfig))
+                    .pipe(gulp.dest(config.icons.destination))
+            }))
             .pipe(notify({
-                title: "Icon Sprite Generated",
-                message: "The SVG icon sprite has been generated.",
+                title: "Icon Sprites Generated",
+                message: "All SVG icon sprites have been generated.",
                 onLast: true
             }));
-
     });
 
-    gulp.task('icons:png-sprite', ['icons:sprite'], function () {
-        return gulp.src(path.join(config.icons.destination, config.paths.icons + '.svg'))
+
+    gulp.task('icons:png-sprites', ['icons:sprites'], function () {
+        return gulp.src(path.join(config.paths.icons, './**/*.svg'))
             .pipe(svg2png())
             .pipe(size({
                 showFiles: true
