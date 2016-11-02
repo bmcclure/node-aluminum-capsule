e8102b1051035c0fc4c10782195a2dc9d822e409
Ben McClure <ben.mcclure@gmail.com>
2016-11-01 23:36:05 -0500

    Add basic asset copying functionality controllable via the config file

---
+++ gulp-tasks/assets.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ben on 11/1/16.
+ */
+var changed = require('gulp-changed');
+
+module.exports = function (gulp, config) {
+    gulp.task('assets', 'assets:copy');
+
+    /**
+     * This task copies assets from other locations if they're different than what's already in the destination
+     */
+    gulp.task('assets:copy', function () {
+        for (var dest in config.assets) {
+            if (config.assets.hasOwnProperty(dest)) {
+                if (config.assets[dest].length > 0) {
+                    gulp.src(config.assets[dest])
+                        .pipe(changed(dest))
+                        .pipe(gulp.dest(dest))
+                }
+            }
+        }
+    })
+};
--- index.js
+++ index.js
@@ -26,6 +26,7 @@ module.exports = {
                 'sass',
                 'scripts',
                 'modernizr',
+                'assets',
                 'browser-sync',
                 'watch',
                 'default'
